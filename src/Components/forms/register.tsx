8ac4872ea4732d3a41222d5237e2e68e3bdbf1d1
Artyom Chshyogolev <artyom.chshyogolev@gmail.com>
2024-02-19 18:32:59 +0600

    Reworked Login&Register forms

---
+++ src/Components/forms/register.tsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@/Components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/Components/ui/form'
+import { Input } from '@/Components/ui/input'
+import axios, { AxiosResponse } from 'axios'
+import { notification } from '@utils'
+import { API } from '@config'
+import LoadingSpinner from '@/Components/loading-spinner'
+
+export function RegisterForm() {
+    // Setups
+    const { t } = useTranslation()
+    const navigate = useNavigate()
+
+
+    // States
+    const [loadingState, setLoadingState] = useState<boolean>(false)
+
+    const formSchema = z.object({
+        firstName: z.string().min(1, { message: t('errors.firstName_req') }).optional(),
+        lastName: z.string().min(1, { message: t('errors.lastName_req') }).optional(),
+        phone: z.string().min(7, { message: t('errors.phone_length') }).includes('+', { message: t('errors.phone_international') }),
+        password: z.string().min(8, { message: t('errors.password_length') }),
+        name: z.string().optional()
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            phone: '+7',
+            password: '',
+            firstName: '',
+            lastName: '',
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        values.name = values.firstName + ' ' + values.lastName
+        values.firstName = undefined
+        values.lastName = undefined
+        setLoadingState(true)
+        axios.post(`${API.baseURL}/users/register`, values).then((response: AxiosResponse) => {
+            if (!response.data.err) {
+                navigate('/auth/login')
+            } else {
+                notification.custom.error(response.data.err)
+            }
+            setLoadingState(false)
+            return
+        }).catch(() => {
+            notification.custom.error(t('errors.too_many_requests'))
+            setLoadingState(false)
+        })
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 w-full md:w-1/3">
+                <div className='flex gap-1.5'>
+                    <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>{t('register.labels.0')}</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>{t('register.labels.1')}</FormLabel>
+                                <FormControl>
+                                    <Input  {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <FormField
+                    control={form.control}
+                    name="phone"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>{t('register.labels.2')}</FormLabel>
+                            <FormControl>
+                                <Input placeholder="+7 123 456 78 90" type='tel' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>{t('register.labels.3')}</FormLabel>
+                            <FormControl>
+                                <Input placeholder="" type='password' {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button className='w-full' type="submit">{loadingState ? <LoadingSpinner /> : t('register.button')}</Button>
+            </form>
+        </Form>
+    )
+}
--- src/Pages/Authentication/Login.tsx
+++ src/Pages/Authentication/Login.tsx
@@ -1,108 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSignIn, useIsAuthenticated } from 'react-auth-kit'
-import axios, { AxiosResponse } from 'axios'
+import { useIsAuthenticated } from 'react-auth-kit'
 import { Toaster } from 'react-hot-toast'
 import { m } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
-import { notification } from '@utils'
-import { API } from '@config'
-import LanguageSwitcher from '@/Components/LanguageSwitcher'
 
-import { Input } from '@/Components/ui/input'
-import { Label } from '@/Components/ui/label'
 import { Button } from '@/Components/ui/button'
-import LoadingSpinner from '@/Components/loading-spinner'
+import { LoginForm } from '@/Components/forms/login'
 
 export default function Login() {
 
 	// Setups
 	const navigate = useNavigate()
-	const signIn = useSignIn()
 	const isAuthenticated = useIsAuthenticated()
 	const { t } = useTranslation()
 
-	// States
-	const [password, setPassword] = React.useState<string>('')
-	const [phone, setPhone] = React.useState<string>('+7')
-	const [loadingState, setLoadingState] = React.useState<boolean>(false)
-
-	// Handlers
-	const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setPassword(event.target.value)
-	}
-
-	const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement> | undefined) => {
-		e && setPhone(e.target.value)
-	}
-
-	// Functions
-	const userSignIn = () => {
-		setLoadingState(true)
-		axios.post(`${API.baseURL}/users/login`, {
-			phone,
-			password,
-		}).then((response: AxiosResponse) => {
-			if (!response.data.err) {
-				if (signIn({
-					token: response.data.token,
-					expiresIn: response.data.expiresIn,
-					tokenType: 'Bearer',
-					authState: response.data.docs,
-				})) {
-					location.reload()
-					notification.custom.success(t('success.login_proccess'))
-				} else {
-					notification.custom.error(t('errors.internal_error'))
-				}
-			} else {
-				const error = response.data.err
-				notification.custom.error(error)
-			}
-			setLoadingState(false)
-		}).catch(() => { notification.custom.error(t('errors.too_many_request')) })
-	}
-
-	function handleOnBlurPhoneInput() {
-		if (!phone.includes('+')) {
-			notification.custom.error(t('errors.phone_international'))
-			return
-		}
-		if (phone.length < 7) {
-			notification.custom.error(t('errors.phone_required'))
-			return
-		}
-	}
-
-	React.useEffect(() => {
+	useEffect(() => {
 		if (isAuthenticated()) {
 			navigate('/pwa')
 		}
-		function confirmpress(event: Event) {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			if (event.key === 'Enter') {
-				event.preventDefault()
-				const loginButton = document.getElementById('loginbtn') as HTMLElement
-				loginButton.click()
-			}
-		}
 		// @ts-expect-error because it is imported from the web
 		ym(96355513, 'hit', window.origin)
-		window.addEventListener('keypress', confirmpress)
-		return () => {
-			window.removeEventListener('keypress', confirmpress)
-		}
 	}, [])
+
 	return (
 		<>
 			<Toaster />
 			<m.div
-				className='flex justify-center items-center h-screen flex-col'
-				initial={{ opacity: 0.1 }}
+				className='flex justify-center items-center h-screen w-screen px-4'
+				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 			>
-				<div className='flex flex-col gap-2'>
+				<div className='flex flex-col gap-2 w-full md:max-w-[20%]'>
 					<div>
 						<h1 className="text-2xl">{t('login.title')}</h1>
 						<p className="text-sm">
@@ -111,23 +40,9 @@ export default function Login() {
 							<Button className='p-0' variant={'link'} onClick={() => { navigate('/auth/register') }}>{t('login.or_create_new_account.1')}</Button>
 						</p>
 					</div>
-					<div className="grid w-full items-center gap-1.5">
-						<Label>{t('login.labels.1')}</Label>
-						<Input placeholder="+7 123 456 7890" value={phone} onChange={handlePhoneChange} onBlur={handleOnBlurPhoneInput} />
-					</div>
-					<div className="grid w-full items-center gap-1.5">
-						<Label htmlFor='user_password'>{t('login.labels.0')}</Label>
-						<Input id='user_password' type="password" onChange={handlePasswordChange} />
-					</div>
-					<Button
-						id="loginbtn"
-						onClick={userSignIn}
-						className='w-full'
-					>
-						{loadingState ? <LoadingSpinner /> :t('login.button')}
-					</Button>
-					<LanguageSwitcher />
+					<LoginForm />
 				</div>
+				
 			</m.div>
 		</>
 	)
